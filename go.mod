f4775fd40ea06a41dc62a6dfe730c6b30208fda4
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-24 02:21:48 +0100

    powerrrr

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Go-Lambda
+
+go 1.17
+
+require github.com/aws/aws-lambda-go v1.28.0
--- main.go
+++ main.go
@@ -1,1 +1,11 @@
-package Go_Lambda
+package main
+
+import "github.com/aws/aws-lambda-go/lambda"
+
+func displayThis() (string, error) {
+	return "Serverless Software Development using AWS Lambda with Go", nil
+}
+
+func main() {
+	lambda.Start(displayThis)
+}
